591bb08dba3f5a03a50c0abfd739d8cc49ac0bbc
timosdev99 <timothyisah4@gmail.com>
2024-08-11 23:12:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return(
+<div className='App'>
+<h1>Hey, Timosdev Here</h1>
+<p>I love coding, physics and math  
+
+  <br/>
+  As a backend developer, my world revolves around the intricate dance of data and logic that powers the digital experiences we interact with daily. But my passion for technology doesn't stop at writing efficient code or optimizing database queries. I'm on a quest to understand the very fabric of computation itself.
+  <br/>
+  <br/>
+  <hr/>
+  <br/>
+  My love for coding is deeply intertwined with my fascination for physics and mathematics. These disciplines form the foundation of computer science, and I find myself constantly amazed by how abstract mathematical concepts translate into the software we use every day.
+  
+</p>
+</div>  
+)
 }
 
 export default App;
