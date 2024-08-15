15af675dcaa5d2002d15c2e23d367d824c1661e4
timosdev99 <timothyisah4@gmail.com>
2024-08-15 20:53:00 +0100

    11t

---
+++ src/random-knowlegde.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import gif from "./gif/Q1Ns.gif";
+import { Link } from "react-router-dom";
+
+function knowlegde() {
+  return (
+    <div className="App">
+      <img src={gif} alt="My gif" />
+      <h1>Hey, Timosdev Here</h1>
+      <p className="para">
+        <ul>
+          <ul>
+            <li className="projects">
+              <b className="projects"> Random knowlegde :)</b>
+            </li>
+            <br/>
+            <ul>
+              <li>
+                <a  className="projects1">Project 1: </a>
+              </li>
+              <br />
+              <li>
+                <a  className="projects2">Project 2: </a>
+              </li>
+              <br />
+              <li>
+                <a  className="projects2">Project 3:</a>
+              </li>
+            </ul>
+
+            <ul>
+            
+              <br/>
+              <li>
+                <Link to="/">Back to Home</Link>
+              </li>
+            </ul>
+          </ul>
+        </ul>
+      </p>
+    </div>
+  );
+}
+
+export default knowlegde;
