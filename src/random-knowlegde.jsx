54ef8373e4b3efab2ebe643a66a71d06b30e6e5c
timosdev99 <timothyisah4@gmail.com>
2024-08-17 19:00:05 +0100

    13t

--- src/random-knowlegde.jsx
+++ src/random-knowlegde.jsx
@@ -17,7 +17,7 @@ function Knowlegde() {
             <br/>
             <ul>
               <li>
-                <a  className="projects1"> 1: About to learn more on neural networks and i'm going to be using brain.js(updating soon) </a>
+                <a  className="projects1"> 1: About to learn more on neural networks and i'm going to be using TensorFlow.js(updating soon) </a>
               </li>
               <br />
               <li>
